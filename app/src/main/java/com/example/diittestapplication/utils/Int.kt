3f919d944d24c7aed19c83636fdcc2d423b721b1
iNex Daniel <inex552@gmail.com>
2021-05-22 18:33:34 +0300

    🗑 Removed unnecessary utils methods

--- app/src/main/java/com/example/diittestapplication/utils/Int.kt
+++ app/src/main/java/com/example/diittestapplication/utils/Int.kt
@@ -7,18 +7,6 @@ import java.util.*
 fun Int.format() =
         String.format("%,d", this).replace(',', ' ')
 
-fun Int.pluralItemsCount(): String {
-    if (this in 11..19)
-        return "$this товаров"
-
-    if (this % 10 == 1)
-        return "$this товар"
-
-    if (this % 10 in 2..4)
-        return "$this товара"
-
-    return "$this товаров"
-}
 
 fun Int.colorFromStatus() = when(this) {
     2 -> R.color.green
--- app/src/main/java/com/example/diittestapplication/utils/Long.kt
+++
@@ -1,8 +0,0 @@
-package com.example.diittestapplication.utils
-
-import java.text.SimpleDateFormat
-import java.util.*
-
-fun Long.timeToFormatedString(): String =
-        SimpleDateFormat("от dd MMMM yyyy, hh:mm", Locale("ru"))
-                .format(Date(this * 1000L))
